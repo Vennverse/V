b09f5554708d367ef74c3520decc5b924823f4db
Vennverse <shubhamdubexskd2001@gmail.com>
2022-04-29 14:47:00 +0000

    COMMIT

--- src/components/MenuItems.jsx
+++ src/components/MenuItems.jsx
@@ -19,10 +19,7 @@ function MenuItems() {
       defaultSelectedKeys={[pathname]}
     >
       <Menu.Item key="/gamify">
-        <NavLink to="/gamify">🚀 Gamify</NavLink>
-      </Menu.Item>
-      <Menu.Item key="/wallet">
-        <NavLink to="/wallet">👛 Wallet</NavLink>
+        <NavLink to="/gamify">🚀 </NavLink>
       </Menu.Item>
       <Menu.Item key="/1inch">
         <NavLink to="/1inch">🏦 Dex</NavLink>
@@ -30,18 +27,10 @@ function MenuItems() {
       <Menu.Item key="onramp">
         <NavLink to="/onramp">💵 Fiat</NavLink>
       </Menu.Item>
-      <Menu.Item key="/erc20balance">
-        <NavLink to="/erc20balance">💰 Balances</NavLink>
-      </Menu.Item>
-      <Menu.Item key="/erc20transfers">
-        <NavLink to="/erc20transfers">💸 Transfers</NavLink>
-      </Menu.Item>
       <Menu.Item key="/nftBalance">
         <NavLink to="/nftBalance">🖼 NFTs</NavLink>
       </Menu.Item>
-      <Menu.Item key="/contract">
-        <NavLink to="/contract">📄 Contract</NavLink>
-      </Menu.Item>
+      
     </Menu>
   );
 }
